27593535a0c7848f471d34cb3569ba037a31f965
MartijnNieuwenhuizen <martijnnieuwenhuizen@icloud.com>
2016-10-08 15:26:49 +0200

    styled the homepage

---
+++ main-nav.php
@@ -0,0 +1,10 @@
+<nav class="main-nav">
+  <?php
+    wp_nav_menu( array(
+        'menu'              => 'Main menu',
+        'theme_location'    => 'header',
+        'depth'             => 2,
+        'menu_class'        => 'nav'
+    ));
+  ?>
+</nav>
